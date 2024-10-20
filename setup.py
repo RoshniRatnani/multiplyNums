9431048864c387f00bccfa2fbaf37b7237730c3a
Roshni Ratnani <roshniratnani2410@gmail.com>
2024-10-20 16:08:10 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
-setup(name='multiplyNums',version='0.2',description='This is a package to multiply 2 numbers',
-long_description="This is a package takes 2 numbers params ans gives the product of those 2 numbers",author="Roshni",
-packages=['multiplyNums'],install_requires=[])
+
+setup(
+    name='multiplyNums',
+    version='0.1.1',  # Incremented version
+    description='This is a package to multiply 2 numbers',
+    long_description="This package takes 2 numbers as parameters and gives the product of those 2 numbers.",
+    author="Roshni",
+    packages=['multiplyNums'],
+    install_requires=[],
+)
